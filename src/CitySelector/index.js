9b659b241a9019876af4983c00c261015e751aae
rizemun <rizeman01@gmail.com>
2019-03-11 11:46:17 +0300

    Add post-request, loading lokalities from request, not load all and filter

--- src/CitySelector/index.js
+++ src/CitySelector/index.js
@@ -29,9 +29,13 @@ function CitySelector(settings) {
         $.getJSON(settings.regionsUrl, (data) => {
             let regions = [].filter.call($(a).children(), elem => elem.classList.contains("CitySelector__regions"));
             let localies = [].filter.call($(a).children(), elem => elem.classList.contains("CitySelector__localities"));
+
             data.forEach(elem => {
                 $(regions).append(createLiElem(elem.title, elem.id, function () {
-                    loadLocalities(localies, settings.localitiesUrl, this.dataset.number);
+
+                    let localityUrl = settings.localitiesUrl + "/" + elem.id;
+                    console.log(localityUrl);
+                    loadLocalities(localies, localityUrl, this.dataset.number, settings.saveUrl);
                     changeSelection(this);
                     changeDestination(elem.id,"");
                 }));
@@ -42,15 +46,15 @@ function CitySelector(settings) {
     });
     this.destroy = function () {
         let target = "#" + settings.elementId;
-        let $target = $(target);
         let selectors = [].filter.call($(target).children(), elem => elem.classList.contains("CitySelector"));
-        //$target.remove(selectors);
-        //selectors.remove();
         $(selectors.pop()).remove();
     };
 }
 
-
+/**
+ * Очищение выделения всех элементов списка и выделение указанного
+ * @param that элемент, который надо сделать выделенным
+ */
 function changeSelection(that) {
     let nodes = $(that).parent().children();
     [].forEach.call(nodes, elem => {
@@ -61,29 +65,36 @@ function changeSelection(that) {
     that.classList.add("_selected");
 }
 
-function loadLocalities(target, url, number) {
-
+/**
+ * Загрузка списка городов
+ * @param target    <Ul> элемент в который записываются города;
+ * @param url       запрос по которому получаем объект
+ * @param number    номер региона
+ */
+function loadLocalities(target, url, number, saveUrl) {
     let loc = $(target);
     loc.empty();
 
     destroySaveButton(loc.parent());
 
     $.getJSON(url, (data) => {
-        data.forEach(region => {
-            if (region.id === number) {
-                region.list.forEach(city => {
-                    loc.append(createLiElem(city, number, function () {
-                        changeSelection(this);
-                        appendSaveButton(loc.parent());
-                        changeDestination(this.dataset.number, this.dataset.title);
-                    }))
-                });
-            }
+        data.list.forEach(city => {
+            loc.append(createLiElem(city, number, function () {
+                changeSelection(this);
+                appendSaveButton(loc.parent(), saveUrl);
+                changeDestination(this.dataset.number, this.dataset.title);
+            }))
         });
-    })
+    });
 }
 
-
+/**
+ * Создание элемента списка регионов/городов и назначение обработчика клика
+ * @param title             Текст элемента (название города)
+ * @param number            id города
+ * @param onClickCallback   функция-обработчик клика на элемент
+ * @returns {HTMLLIElement} элемент списка
+ */
 function createLiElem(title, number, onClickCallback) {
     let liElem = document.createElement("li");
     liElem.classList.add("CitySelector__item");
@@ -99,12 +110,20 @@ function createLiElem(title, number, onClickCallback) {
 /*
 Используется паттерн "синглтон"
  */
-function appendSaveButton(target) {
+/**
+ * Создание и добавление кнопки сохранения к экземпляру модуля.
+ * так же здесь создается обработчик нажатия на эту кнопку
+ * @param target    элемент экземпляра модуля в который добавляем кнопку.
+ */
+function appendSaveButton(target, saveUrl) {
     let buttonClass = "CitySelector__save";
     let $buttonClass = "." + buttonClass;
     let buttonElem = [].filter.call(target.children(), elem => elem.classList.contains(buttonClass));
 
     if (buttonElem.length !== 0) {
+        if(buttonElem[0].disabled === true){
+            buttonElem[0].disabled = false;
+        }
         return;
     }
     let button = document.createElement("input");
@@ -121,23 +140,52 @@ function appendSaveButton(target) {
         let currentRegion = [].filter.call(regions, elem => elem.classList.contains("_selected"))[0];
         let currentLocation = [].filter.call(locations, elem => elem.classList.contains("_selected"))[0];
         let result ={
-            region: currentRegion.dataset.title,
-            locality: currentLocation.dataset.title,
-            id: currentRegion.dataset.number
+            region: currentRegion.dataset.number,
+            locality: currentLocation.dataset.title
         };
+
+
+
+        console.log("Синхронный POST запрос");
+        console.log("сейчас будет ошибка что нельзя из основного потока запускать синхронный запрос");
+
+        $.ajax({
+            type: 'POST',
+            url: saveUrl,
+            data: result,
+            success: ()=>{
+                console.log("Ответ на запрос получен");
+            },
+            async: false
+        });
+
+
         console.log(result);
     });
 }
+
+/**
+ * Деактивирует кнопку сохранения
+ * @param   target указатель на ту удаляемый элемент.
+ */
 function destroySaveButton(target){
+
+
 
     let buttonClass = "CitySelector__save";
     let buttonElem = [].filter.call(target.children(), elem => elem.classList.contains(buttonClass));
     if(buttonElem.length > 0){
-        $(buttonElem).remove();
+        buttonElem[0].disabled = true;
+        // $(buttonElem).remove();
     }
 
 }
 
+/**
+ * Изменение информации в элементе вверху, справа
+ * @param id        номер региона
+ * @param locality  название города
+ */
 function changeDestination(id, locality){
     let target = {
         id: $("#regionText")[0],
